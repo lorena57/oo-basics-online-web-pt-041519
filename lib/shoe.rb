4ea588004457432a61fc5e7d88b403b2757de35a
github username <github email address>
2019-08-28 19:08:42 -0700

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,26 @@
 # Make your shoe class here!
+
+require "pry"
+
+class Shoe
+
+    attr_accessor :color, :size, :material, :condition
+    
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def cobble
+
+        @condition = "new"
+    binding.pry
+        puts "Your shoe is as good as new!"
+        
+    end
+
+  
+end
